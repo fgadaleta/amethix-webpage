7eb99db10e7ca87fb10dd6482b4d086020b079ab
Abdul Rehman <rehmankhld@gmail.com>
2021-11-09 19:53:58 +0500

    pixelation duration

--- assets/js/script.js
+++ assets/js/script.js
@@ -279,7 +279,7 @@
 
 
 
-    function animatePixel(imgElem, speed) {
+    function animatePixel(imgElem, speed=100) {
       // Grab the Canvas and Drawing Context
       var canvas = document.createElement('canvas');
       $(canvas).addClass("toBePixeled");
@@ -359,6 +359,12 @@
       }
     }
 
-    animatePixel(document.querySelector(".imgpx"), 100);
+    $(".imgpx").each(function(idx, elm){
+      var duration = 1;
+      if($(elm).data("duration")){
+        duration = $(elm).data("duration");
+      }
+      animatePixel(elm, duration*100);
+    });
   });
 })(jQuery);
